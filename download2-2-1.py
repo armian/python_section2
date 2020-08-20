8ced1916c089d07a343bf8beef7eb289c9470ec8
ARMIAN_MBP <armian@choiyunsoo-KISTI.local>
2020-08-20 19:14:32 +0900

    new comments

--- download2-2-1.py
+++ download2-2-1.py
@@ -1,5 +1,7 @@
 import urllib.request as dw
 
+# new comments
+
 imgUrl = "https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2F20150823_231%2Fcodn9_14403263840439uB4A_JPEG%2Ftumblr_n0forjSMuW1qgyg6yo1_540.jpg&type=sc960_832"
 htmlURL = "http://google.com"
 
